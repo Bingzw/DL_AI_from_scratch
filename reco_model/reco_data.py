3fbf6cc93915fde51d651ccc7926ff960b550962
bwang7 <bwang7@ebay.com>
2024-04-14 01:18:54 -0700

    add dlrm model

--- reco_model/reco_data.py
+++ reco_model/reco_data.py
@@ -1,37 +1,60 @@
-import torch
-import os
 import pandas as pd
+import torch
 import torch.utils.data as data
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, random_split
 
 
 class DLRMDataSet(data.Dataset):
     def __init__(self, data_path):
-        super.__init__()
-        self.data = pd.read_csv(data_path).fillna(0)
+        super().__init__()
+        self.data = pd.read_csv(data_path)
+        self.features = self.data.iloc[:, :-1]
+        self.labels = self.data.iloc[:, -1]
+        self.sparse_columns = ['C' + str(i) for i in range(1, 27)]
+        self.dense_columns = ['I' + str(i) for i in range(1, 14)]
+        self.sparse_features = self.features[self.sparse_columns]
+        self.dense_features = self.features[self.dense_columns]
+        self.embedding_cardinality = [self.features[column].nunique() for column in self.sparse_columns]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        dense_feature = torch.tensor(self.dense_features.iloc[idx].values, dtype=torch.float32)
+        sparse_feature = torch.tensor(self.sparse_features.iloc[idx].values, dtype=torch.long)
+        # Get label
+        label = torch.tensor(self.labels.iloc[idx])
+
+        return dense_feature, sparse_feature, label
 
 
+class DLRMDataModule(pl.LightningDataModule):
+    def __init__(self, data_path, batch_size=32, hidden_dim=10, shuffle=True):
+        """
+        :param data_path: path of the raw data
+        :param batch_size: batch_size
+        :param hidden_dim: the hidden dimension of the embedding layer for sparse features
+        :param shuffle: shuffle the data or not
+        """
+        super().__init__()
+        self.data = DLRMDataSet(data_path)
+        self.embedding_sizes = [(num_categories, hidden_dim) for num_categories in self.data.embedding_cardinality]
+        self.batch_size = batch_size
+        self.shuffle = shuffle
 
+    def setup(self, stage=None):
+        train_size = int(0.6 * len(self.data))
+        val_size = int(0.2 * len(self.data))
+        test_size = len(self.data) - train_size - val_size
+        self.train_data, self.val_data, self.test_data = random_split(self.data, [train_size, val_size, test_size])
 
+    def train_dataloader(self):
+        return DataLoader(self.train_data, batch_size=self.batch_size, shuffle=self.shuffle)
 
+    def val_dataloader(self):
+        return DataLoader(self.val_data, batch_size=self.batch_size, shuffle=False)
 
+    def test_dataloader(self):
+        return DataLoader(self.test_data, batch_size=self.batch_size, shuffle=False)
 
-if __name__ == '__main__':
-    current_directory = os.getcwd()
-    parent_directory = os.path.dirname(current_directory)
-    train_path = os.path.join(parent_directory, "data/reco_data/train.csv")
-    train_data = pd.read_csv(train_path)
-
-    # print
-    shuffled_df = train_data.sample(frac=1, random_state=42)
-
-    # Split the shuffled DataFrame into train and validation sets
-    train_ratio = 0.8  # 80% of the data for training
-    train_size = int(train_ratio * len(shuffled_df))
-    train_df = shuffled_df[:train_size]
-    val_df = shuffled_df[train_size:]
-
-
-    print(train_df.shape)
-    print(val_df.shape)
-    train_df.to_csv(os.path.join(parent_directory, "data/reco_data/new_train.csv"), index=False)
-    val_df.to_csv(os.path.join(parent_directory, "data/reco_data/val.csv"), index=False)
