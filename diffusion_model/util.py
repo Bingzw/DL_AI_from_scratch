fa9a1b0b6792cc3f5c09999b2586ffdffbf7862b
bwang7 <bwang7@ebay.com>
2024-05-17 16:29:26 -0700

    fix generating function

--- diffusion_model/util.py
+++ diffusion_model/util.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import imageio
 import einops
+from tqdm import tqdm
 
 
 def set_seed(seed):
@@ -68,6 +69,7 @@ def show_forward(ddpm, loader, device):
 
 
 def generate_new_images(ddpm, n_samples=16, device=None, frames_per_gif=100, gif_name="sampling.gif", c=1, h=28, w=28):
+    """Given a DDPM model, a number of samples to be generated and a device, returns some newly generated samples"""
     frame_idxs = np.linspace(0, ddpm.n_steps, frames_per_gif).astype(np.uint)
     frames = []
 
@@ -75,13 +77,13 @@ def generate_new_images(ddpm, n_samples=16, device=None, frames_per_gif=100, gif
         if device is None:
             device = ddpm.device
 
-        # starting from random noise
+        # Starting from random noise
         x = torch.randn(n_samples, c, h, w).to(device)
 
-        for idx, t in enumerate(list(range(ddpm.n_steps))[::-1]):
-            # estimate the noise to be removed
-            time_sensor = (torch.ones(n_samples, 1) * t).to(device).long()
-            eta_theta = ddpm.backward(x, time_sensor)
+        for idx, t in tqdm(enumerate(list(range(ddpm.n_steps))[::-1])):
+            # Estimating noise to be removed
+            time_tensor = (torch.ones(n_samples, 1) * t).to(device).long()
+            eta_theta = ddpm.backward(x, time_tensor)
 
             alpha_t = ddpm.alphas[t]
             alpha_t_bar = ddpm.alpha_bars[t]
@@ -90,41 +92,42 @@ def generate_new_images(ddpm, n_samples=16, device=None, frames_per_gif=100, gif
 
             if t > 0:
                 z = torch.randn(n_samples, c, h, w).to(device)
-                # calculate beta_tilda_t
+
                 beta_t = ddpm.betas[t]
-                prev_alpha_t_bar = ddpm.alpha_bars[t - 1] if t > 0 else ddpm.alphas[0]
+                prev_alpha_t_bar = ddpm.alpha_bars[t-1] if t > 0 else ddpm.alphas[0]
                 beta_tilda_t = ((1 - prev_alpha_t_bar)/(1 - alpha_t_bar)) * beta_t
                 sigma_t = beta_tilda_t.sqrt()
-                # adding noise using Langevin dynamics
+
+                # Adding some more noise like in Langevin Dynamics fashion
                 x = x + sigma_t * z
 
-                # Adding frames to the GIF
-                if idx in frame_idxs or t == 0:
-                    # Putting digits in range [0, 255]
-                    normalized = x.clone()
-                    for i in range(len(normalized)):
-                        normalized[i] -= torch.min(normalized[i])
-                        normalized[i] *= 255 / torch.max(normalized[i])
+            # Adding frames to the GIF
+            if idx in frame_idxs or t == 0:
+                # Putting digits in range [0, 255]
+                normalized = x.clone()
+                for i in range(len(normalized)):
+                    normalized[i] -= torch.min(normalized[i])
+                    normalized[i] *= 255 / torch.max(normalized[i])
 
-                    # Reshaping batch (n, c, h, w) to be a (as much as it gets) square frame
-                    frame = einops.rearrange(normalized, "(b1 b2) c h w -> (b1 h) (b2 w) c", b1=int(n_samples ** 0.5))
-                    frame = frame.cpu().numpy().astype(np.uint8)
+                # Reshaping batch (n, c, h, w) to be a (as much as it gets) square frame
+                frame = einops.rearrange(normalized, "(b1 b2) c h w -> (b1 h) (b2 w) c", b1=int(n_samples ** 0.5))
+                frame = frame.cpu().numpy().astype(np.uint8)
 
-                    # Rendering frame
-                    frames.append(frame)
+                # Rendering frame
+                frames.append(frame)
 
-            # Storing the gif
-            with imageio.get_writer(gif_name, mode="I") as writer:
-                for idx, frame in enumerate(frames):
-                    rgb_frame = np.repeat(frame, 3, axis=2)
-                    writer.append_data(rgb_frame)
+    # Storing the gif
+    with imageio.get_writer(gif_name, mode="I") as writer:
+        for idx, frame in enumerate(frames):
+            rgb_frame = np.repeat(frame, 3, axis=2)
+            writer.append_data(rgb_frame)
 
-                    # Showing the last frame for a longer time
-                    if idx == len(frames) - 1:
-                        last_rgb_frame = np.repeat(frames[-1], 3, axis=2)
-                        for _ in range(frames_per_gif // 3):
-                            writer.append_data(last_rgb_frame)
-            return x
+            # Showing the last frame for a longer time
+            if idx == len(frames) - 1:
+                last_rgb_frame = np.repeat(frames[-1], 3, axis=2)
+                for _ in range(frames_per_gif // 3):
+                    writer.append_data(last_rgb_frame)
+    return x
 
 
 
